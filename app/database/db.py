c68fac096b5302c7d6e569e6a2c20fd3e97a97cf
PranavAggarwal422 <pranav24422@iiitd.ac.in>
2026-03-26 20:04:41 +0530

    Fix MySQL connection configuration

--- app/database/db.py
+++ app/database/db.py
@@ -7,9 +7,10 @@ def get_db_connection() :
             host = current_app.config["MYSQL_HOST"], 
             user = current_app.config["MYSQL_USER"], 
             password = current_app.config["MYSQL_PASSWORD"], 
-            database = current_app.config["MYSQL_DB"]
+            database = current_app.config["MYSQL_DB"],
+            port= current_app.config["MYSQL_PORT"]
         )
         return connection 
     
     except mysql.connector.Error as err : 
-        return None 
+        raise err 
